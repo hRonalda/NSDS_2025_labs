100d79dc9d9e3cc44639b322d2a2541cf897e7b0
Rong H <rongh@RongtekiMacBook-Air.local>
2025-11-07 11:46:37 +0100

    Exercise 4 finished

--- src/main/java/com/messageBuffer_exer4/SleepWakeupClient.java
+++ src/main/java/com/messageBuffer_exer4/SleepWakeupClient.java
@@ -1,4 +1,45 @@
 package com.messageBuffer_exer4;
 
-public class SleepWakeupClient {
+import akka.actor.*;
+
+public class SleepWakeupClient extends AbstractActor {
+    public static void main(String[] args) {
+        // 1. Create ActorSystemc
+        final ActorSystem system = ActorSystem.create("SleepWakeupClient");
+
+        // 2. Create server actor and client actor
+        final ActorRef server = system.actorOf(Props.create(SleepWakeupServerActor.class), "server");
+        final ActorRef client = system.actorOf(Props.create(SleepWakeupClient.class), "client");
+
+        // 3. test the messages
+        server.tell(new SleepWakeupServerActor.TextMsg("Message 1"), client);
+        server.tell(new SleepWakeupServerActor.TextMsg("Message 2"), client);
+        server.tell(new SleepWakeupServerActor.SleepMsg(), client);
+        server.tell(new SleepWakeupServerActor.TextMsg("Message 3"), client);
+        server.tell(new SleepWakeupServerActor.TextMsg("Message 4"), client);
+        server.tell(new SleepWakeupServerActor.WakeupMsg(), client);
+        server.tell(new SleepWakeupServerActor.TextMsg("Message 5"), client);
+        server.tell(new SleepWakeupServerActor.TextMsg("Message 6"), client);
+
+        // keep system alive
+        try {
+            System.in.read();
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
+        system.terminate();
+
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(String.class, msg -> System.out.println("client received reply: " + msg))
+                .build();
+    }
+
+
+
+
 }
--- src/main/java/com/messageBuffer_exer4/SleepWakeupServerActor.java
+++ src/main/java/com/messageBuffer_exer4/SleepWakeupServerActor.java
@@ -1,5 +1,52 @@
 package com.messageBuffer_exer4;
 
-public class SleepWakeupServerActor {
+import akka.actor.AbstractActorWithStash;
 
+public class SleepWakeupServerActor extends AbstractActorWithStash {
+
+    private boolean isSleeping = false;
+
+    // Three types of messages
+    public static final class TextMsg {
+        public final String text;
+        public TextMsg(String text) {
+            this.text = text;
+        }
+    }
+
+    // don’t carry data (just signals), so don’t need fields.
+    public static final class SleepMsg { }
+
+    public static final class WakeupMsg{ }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(TextMsg.class, this::onText)
+                .match(SleepMsg.class, this::onSleep)
+                .match(WakeupMsg.class, this::onWakeup)
+                .build();
+    }
+
+
+    void onText(TextMsg msg) {
+        if (isSleeping) {
+             System.out.println("Stash message: " + msg.text);
+             stash();
+        } else {
+            System.out.println("Received message: " + msg.text);
+            getSender().tell("Echo from server: " + msg.text, getSelf());
+        }
+    }
+
+    void onSleep(SleepMsg msg) {
+        System.out.println("Sleeping");
+        isSleeping = true;
+    }
+
+    void onWakeup(WakeupMsg msg) {
+        System.out.println("Waking up");
+        isSleeping = false;
+        unstashAll();
+    }
 }
